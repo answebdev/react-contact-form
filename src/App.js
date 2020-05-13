7129260d60175d2a79ee6b1267d8c80542bce7d4
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 10:28:30 -0700

    added Thanks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ContactForm from './components/pages/ContactForm';
+import Thanks from './components/pages/Thanks';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <ContactForm />
+      <Router>
+        <Switch>
+          <Route exact path='/contact' component={ContactForm} />
+          <Route exact path='/thanks' component={Thanks} />
+          {/* <Route component={Error} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Thanks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Thanks = () => {
+  return (
+    <div>
+      <div className='content'>
+        <p>Thank you.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Thanks;
