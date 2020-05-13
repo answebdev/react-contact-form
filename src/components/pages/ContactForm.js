7041f5704d27d4de6b38466a91d82633300a03e2
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 15:18:29 -0700

    updated contact form

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -24,21 +24,20 @@ export class ContactForm extends Component {
               method='POST'
               data-netlify='true'
             >
+              <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control type='name' placeholder='Name' />
+              </Form.Group>
               <Form.Group controlId='formBasicEmail'>
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
+                <Form.Control type='email' placeholder='email@example.com' />
                 <Form.Text className='text-muted'>
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
-
-              <Form.Group controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Password' />
-              </Form.Group>
               <Form.Group controlId='exampleForm.ControlTextarea1'>
                 <Form.Label>Example textarea</Form.Label>
-                <Form.Control as='textarea' rows='3' />
+                <Form.Control as='textarea' rows='5' />
               </Form.Group>
               <div className='field'>
                 <div data-netlify-recaptcha='true'></div>
