0ca1fdf80544016e88916ae4bf643732e21345f6
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 10:35:09 -0700

    removed redirects

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <ContactForm />
       <Router>
         <Switch>
           <Route exact path='/contact' component={ContactForm} />
