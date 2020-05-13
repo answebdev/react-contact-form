2c587e3e95ea8aecfe5c13ca9d461253b1928efd
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 08:59:18 -0700

    added Thank You component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactForm from './components/pages/ContactForm';
+import ThankYou from './components/pages/ThankYou';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <ContactForm />
+      <ThankYou />
     </div>
   );
 }
---
+++ src/components/pages/ThankYou.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ThankYou = () => {
+  return (
+    <div>
+      <h1>Thank You</h1>
+
+      <p>Your email has been sent.</p>
+    </div>
+  );
+};
+
+export default ThankYou;
