4c6f72c4da50e7752d532b1af6f9728ed934d79d
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 17:20:11 -0700

    updated code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './components/pages/About';
 import Success from './components/pages/Success';
 import Error from './components/pages/Error';
 import Navigation from './components/layout/Navigation';
-import Footer from './components/pages/Footer';
+import Footer from './components/layout/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
