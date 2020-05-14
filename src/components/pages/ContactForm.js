50523e0c3e3b10c37bb61a07d0787784d4c735cb
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-13 21:01:33 -0700

    cleaned up code

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -10,52 +10,61 @@ export class ContactForm extends Component {
           <Helmet>
             <title>Contact</title>
           </Helmet>
-          <h1>Contact Form</h1>
 
-          <div className='content'>
-            <p>
-              Feel free to contact us with any questions. Submit your name,
-              email, and a short message, and we'll get back to you as soon as
-              possible.{' '}
-              <a id='click' href='/success'>
-                Click Here
-              </a>
-            </p>
-            <Form
-              action='/success'
-              name='react-contact-form'
-              method='POST'
-              data-netlify='true'
-            >
-              <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='name' placeholder='Name' />
-              </Form.Group>
-              <Form.Group controlId='formBasicEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type='email' placeholder='email@example.com' />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId='exampleForm.ControlTextarea1'>
-                <Form.Label>Message</Form.Label>
-                <Form.Control as='textarea' rows='5' />
-              </Form.Group>
-              <div className='field'>
-                <div data-netlify-recaptcha='true'></div>
+          <div className='row'>
+            <div className='col-md-3'></div>
+            <div className='col-md-6'>
+              <h1>Contact Form</h1>
+              <div className='content'>
+                <p>
+                  Feel free to contact us with any questions. Submit your name,
+                  email, and a short message, and we'll get back to you as soon
+                  as possible.{' '}
+                  <a id='click' href='/success'>
+                    Click Here
+                  </a>
+                </p>
+                <Form
+                  action='/success'
+                  name='react-contact-form'
+                  method='POST'
+                  data-netlify='true'
+                >
+                  <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type='name' placeholder='Name' />
+                  </Form.Group>
+                  <Form.Group controlId='formBasicEmail'>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type='email'
+                      placeholder='email@example.com'
+                    />
+                    <Form.Text className='text-muted'>
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+                  <Form.Group controlId='exampleForm.ControlTextarea1'>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as='textarea' rows='5' />
+                  </Form.Group>
+                  <div className='field'>
+                    <div data-netlify-recaptcha='true'></div>
+                  </div>
+                  <Button
+                    className='actions'
+                    variant='primary'
+                    type='submit'
+                    value='Send Message'
+                    id='submit-btn'
+                  >
+                    Send Message
+                  </Button>
+                </Form>
+                <br />
               </div>
-              <Button
-                className='actions'
-                variant='primary'
-                type='submit'
-                value='Send Message'
-                id='submit-btn'
-              >
-                Send Message
-              </Button>
-            </Form>
-            <br />
+            </div>
+            <div className='col-md-3'></div>
           </div>
         </div>
       </Fragment>
