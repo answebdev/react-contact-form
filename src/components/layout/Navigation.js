33ae6795db1ccad5d27f43d2db347394fbe2532c
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-15 17:19:33 -0700

    added alternate navbar and media queries

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,68 +1,109 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import Scroll from 'react-scroll';
+// import logo from '../layout/react-logo.png';
 
-export class Navigation extends Component {
-  render() {
-    return (
-      <div className='header'>
-        <Navbar className='navbar-dark' collapseOnSelect expand='lg'>
-          <Navbar.Brand
-            id='app-name'
-            onSelect={() =>
-              Scroll.scrollTo('home', {
-                smooth: true,
-                offset: -70,
-                duration: 500,
-              })
-            }
-          >
-            <span>
-              <a id='brand-link' href='/'>
-                Practice React App
-              </a>{' '}
-            </span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Link
-                className='nav-link'
-                to='/'
-                style={{
-                  paddingLeft: '.5rem',
-                  paddingRight: '.5rem',
-                }}
-              >
-                Home
-              </Link>
-              <Link
-                className='nav-link'
-                to='/contact'
-                style={{
-                  paddingLeft: '.5rem',
-                  paddingRight: '.5rem',
-                }}
-              >
-                Contact
-              </Link>
-              <Link
-                className='nav-link'
-                to='/about'
-                style={{
-                  paddingLeft: '.5rem',
-                  paddingRight: '.5rem',
-                }}
-              >
-                About
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+const Navigation = ({ title }) => {
+  return (
+    <Fragment>
+      <div className='navbar bg-primary'>
+        <a href='/'>
+          <p className='nav-title'>
+            {/* <img id='app-logo' src={logo} alt='React Logo' />
+            &nbsp; */}
+            {title}
+          </p>
+        </a>
+        <div id='links'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    );
-  }
-}
+    </Fragment>
+  );
+};
+
+Navigation.defaultProps = {
+  title: 'Practice React App',
+};
 
 export default Navigation;
+
+// NAVBAR WITH HAMBURGER COLLAPSE:
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import { Navbar, Nav } from 'react-bootstrap';
+// import Scroll from 'react-scroll';
+
+// export class Navigation extends Component {
+//   render() {
+//     return (
+//       <div className='header'>
+//         <Navbar className='navbar-dark' collapseOnSelect expand='lg'>
+//           <Navbar.Brand
+//             id='app-name'
+//             onSelect={() =>
+//               Scroll.scrollTo('home', {
+//                 smooth: true,
+//                 offset: -70,
+//                 duration: 500,
+//               })
+//             }
+//           >
+//             <span>
+//               <a id='brand-link' href='/'>
+//                 Practice React App
+//               </a>{' '}
+//             </span>
+//           </Navbar.Brand>
+//           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+//           <Navbar.Collapse id='responsive-navbar-nav'>
+//             <Nav className='ml-auto'>
+//               <Link
+//                 className='nav-link'
+//                 to='/'
+//                 style={{
+//                   paddingLeft: '.5rem',
+//                   paddingRight: '.5rem',
+//                 }}
+//               >
+//                 Home
+//               </Link>
+//               <Link
+//                 className='nav-link'
+//                 to='/contact'
+//                 style={{
+//                   paddingLeft: '.5rem',
+//                   paddingRight: '.5rem',
+//                 }}
+//               >
+//                 Contact
+//               </Link>
+//               <Link
+//                 className='nav-link'
+//                 to='/about'
+//                 style={{
+//                   paddingLeft: '.5rem',
+//                   paddingRight: '.5rem',
+//                 }}
+//               >
+//                 About
+//               </Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Navbar>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Navigation;
