db2937f662859345d04e84aa19a0793d0280ae3b
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-19 15:58:30 -0700

    updated landing page code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
-// import woman from '../../img/dz.jpg';
-// import skirt from '../../img/skirt.png';
+import skirt from '../../img/skirt.png';
 
 const Home = () => {
   return (
@@ -10,14 +9,14 @@ const Home = () => {
       <Helmet>
         <title>Reach Playground</title>
         <style type='text/css'>{`
-       * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-      }
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
 
         body {
-            background-color: #3e63bd;
+          background-color: #3e63bd;
         }
 
         html {
@@ -32,14 +31,15 @@ const Home = () => {
           overflow: hidden;
           padding: 0;
         }
+        
         .navbar,
         .footer {
           display:none;
         }
 
         h1 {
-            font-family: 'Dancing Script', cursive;
-            color: #ffffff;
+          font-family: 'Dancing Script', cursive;
+          color: #ffffff;
         }
 
         header {
@@ -54,22 +54,7 @@ const Home = () => {
           transform-style: preserve-3d;
           perspective: 100px;
         }
-        
-        header:before {
-         
-          // background: url('../../img/skirt.png');
-          background: url('https://images.pexels.com/photos/144474/pexels-photo-144474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-          background-size: cover, cover;
-          background-position: center, center;
-          content: '';
-          position: absolute;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          z-index: -1;
-        }
-
+      
         #welcome-msg {
           // font-size: 50px;
           border: 1px solid #ffffff;
@@ -78,8 +63,6 @@ const Home = () => {
         }
 
         #welcome-msg:hover {
-          // background: #ffffff;
-          // opacity: 0.7;
           background: rgba(255,255,255,0.5);
         }
 
@@ -91,27 +74,19 @@ const Home = () => {
       <Container className='container' fluid>
         <Row>
           <Col md={12}>
-            <header>
-              {/* <h1 className='text-center'>Landing Page</h1> */}
-              {/* <img
-                className='header'
-                src={woman}
-                class='img-fluid mb-3'
-                alt=''
-              /> */}
-              {/* <div className='header'></div> */}
+            {/* Put background image file in Header: */}
+            <header
+              style={{
+                backgroundImage: `url(${skirt})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
               <h1 id='welcome-msg'>
                 <a id='welcome' href='/blog'>
                   Welcome
                 </a>
               </h1>
-
-              {/* <a
-                href='/blog'
-                class='btn btn-primary btn-lg'
-                role='button'
-                aria-pressed='true'
-              > */}
             </header>
           </Col>
         </Row>
