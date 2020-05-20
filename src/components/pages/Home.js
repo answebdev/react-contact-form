fe7c6a4ccbcc0d08ad739217f48765bf0e1b31cd
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-19 17:58:00 -0700

    cleaned up code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,21 +10,6 @@ const Home = () => {
       <Helmet>
         <title>React Playground</title>
         <style type='text/css'>{`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-          padding: 0;
-        }
-
-        body {
-          background-color: #3e63bd;
-        }
-
-        html {
-          height: 100%;
-          width: 100%;
-        }
-
         .container {
           max-width: 100%;
           height: 100%;
@@ -36,24 +21,6 @@ const Home = () => {
         .navbar,
         .footer {
           display:none;
-        }
-
-        h1 {
-          font-family: 'Dancing Script', cursive;
-          color: #ffffff;
-        }
-
-        header {
-          align-items: center;
-          display: flex;
-          font-size: 18px;
-          height: 100vh;
-          justify-content: center;
-          overflow: hidden;
-          position: relative;
-          text-align: center;
-          transform-style: preserve-3d;
-          perspective: 100px;
         }
 
         // header:before {
@@ -69,20 +36,7 @@ const Home = () => {
         //   z-index: -1;
         // }
       
-        #welcome-msg {
-          // font-size: 50px;
-          border: 1px solid #ffffff;
-          padding: 10px;
-          border-radius: 5px;
-        }
-
-        #welcome-msg:hover {
-          background: rgba(255,255,255,0.5);
-        }
-
-        #welcome:hover {
-          color: #3e63bd;
-        }
+       
     `}</style>
       </Helmet>
       <Container className='container' fluid>
@@ -90,6 +44,7 @@ const Home = () => {
           <Col md={12}>
             {/* Put background image file in Header: */}
             <header
+              id='landing-header'
               style={{
                 backgroundImage: `url(${skirt})`,
                 backgroundSize: 'cover',
