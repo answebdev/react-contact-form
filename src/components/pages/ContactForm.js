ab5ce37617880c1fe5eb2dc715a6ee5e97e04266
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-21 18:06:27 -0700

    added html form

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -27,6 +27,7 @@ export class ContactForm extends Component {
                     <span style={{ marginLeft: '-10px' }}>.</span>
                   </p>
                   <Form
+                    action='/success'
                     name='react-contact-form'
                     method='POST'
                     data-netlify='true'
