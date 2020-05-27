3057cb8befc178985ee4a4d5293d383f58a70243
ANS <36783010+answebdev@users.noreply.github.com>
2020-05-26 17:15:42 -0700

    added share buttons

--- src/components/pages/ReactExamples.js
+++ src/components/pages/ReactExamples.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { Container, Col, Row } from 'react-bootstrap';
+import {
+  Facebook,
+  Twitter,
+  Email,
+  Google,
+  Pinterest,
+} from 'react-sharingbuttons';
+import 'react-sharingbuttons/dist/main.css';
 
 class ReactExamples extends Component {
   render() {
+    const url = 'https://github.com/caspg/react-sharingbuttons';
+    const shareText = 'Check this site!';
     return (
       <div>
         <Helmet>
@@ -13,8 +23,16 @@ class ReactExamples extends Component {
           <h3>React Examples Page</h3>
           <p>Welcome to the React Examples Page.</p>
           <hr />
+
           <Row>
             <Col md={12}>
+              <div>
+                <Facebook url={url} />
+                <Twitter url={url} shareText={shareText} />
+                <Email url={url} subject='React Examples Page' />
+                <Google url={url} />
+                <Pinterest url={url} shareText={shareText} mediaSrc={url} />
+              </div>
               <br />
               <div id='back-div'>
                 <a
